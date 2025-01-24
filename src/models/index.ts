c6a79c5a3f1210a299da4cbefe1e72e81de77fc5
Logan <logankrieger317@gmail.com>
2025-01-23 19:48:10 -0600

    updating index

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,10 +7,26 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
 
 let sequelize: Sequelize;
-if (config.use_env_variable) {
+if (process.env.DATABASE_URL) {
+  // Use DATABASE_URL if available (production)
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else if (config.use_env_variable && process.env[config.use_env_variable]) {
+  // Use other environment variable if specified
   sequelize = new Sequelize(process.env[config.use_env_variable] as string, config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // Use local config
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    dialect: 'postgres'
+  });
 }
 
 const Product = initProduct(sequelize);
